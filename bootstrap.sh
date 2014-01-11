203480f4ae9db57d53fef03ab39a58c6770b246a
posbo <paul.osborne.uk@gmail.com>
2014-01-11 15:24:56 +0000

    added spamassassin and dovecot

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 HOSTNAME="mail.zsoc.com"
 DOMAIN="zsoc.com"
 
+# SPAMASSASSIN CONFIGURATION
+SA_USER="debian-spamd"
+SA_CHILDREN=2
+
 sudo apt-get update
 sudo apt-get install -y python-software-properties
  
@@ -46,18 +50,36 @@ sudo echo "@${DOMAIN}   catchall@${DOMAIN}" > /etc/postfix/virtual
 sudo echo "${DOMAIN}    OK" > /etc/postfix/virtual-mailbox-domains
 
 # setup virtual mailbox users
-sudo echo "oz@${DOMAIN} OK" > /etc/postfix/virtual-mailbox-users
+sudo echo "no-reply@${DOMAIN} OK" > /etc/postfix/virtual-mailbox-users
 
 # generate databases
 postmap /etc/postfix/virtual
 postmap /etc/postfix/virtual-mailbox-domains
 postmap /etc/postfix/virtual-mailbox-users
 
-# 
-sudo echo -e "dovecot   unix  -       n       n       -       -       pipe" >> /etc/postfix/master.cf
+# connect dovecot and postfix
+sudo echo 'dovecot   unix  -       n       n       -       -       pipe' >> /etc/postfix/master.cf
 sudo echo -e "\tflags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/deliver" >> /etc/postfix/master.cf
 sudo echo -e "\t-f \${sender} -d \${recipient}" /etc/postfix/master.cf >> /etc/postfix/master.cf
 
 # install SpamAssassin
 sudo apt-get install -y spamass-milter
 sudo mkdir /var/run/spamassassin
+
+# configure spamassasin
+sudo sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/spamassassin
+sudo sed -i 's/CRON=0/CRON=1/g' /etc/default/spamassassin
+sudo echo "SAHOME=\"/var/lib/spamassassin\"" >> /etc/default/spamassassin
+sudo sed -i "/OPTIONS=/c\OPTIONS=\"--username ${SA_USER} --nouser-config --max-children ${SA_CHILDREN} --helper-home-dir \${SAHOME} --socketpath=/var/run/spamassassin/spamd.sock --socketowner=${SA_USER} --socketgroup=${SA_USER} --socketmode=0660\"" /etc/default/spamassassin
+sudo sed -i '/PIDFILE=/c\PIDFILE="/var/run/spamassassin/spamd.pid"' /etc/default/spamassassin
+
+sudo sed -i '/OPTIONS=/c\OPTIONS="-u spamass-milter -m -I -i 127.0.0.1 -- --socket=/var/run/spamassassin/spamd.sock"' /etc/default/spamass-milter
+sudo usermod -a -G debian-spamd spamass-milter
+
+# connect postfix and spamassasin
+sudo echo 'smtpd_milters = unix:/spamass/spamass.sock' >> /etc/postfix/main.cf
+sudo echo 'milter_connect_macros = j {daemon_name} v {if_name} _' >> /etc/postfix/main.cf
+sudo echo 'milter_default_action = tempfail' >> /etc/postfix/main.cf
+
+# install dovecot
+sudo apt-get install -y dovecot-imapd
